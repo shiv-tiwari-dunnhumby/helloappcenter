343f7b6b8d0aa0613583a88462f01073fec16606
Shivendra Tiwari <shivendra.tiwari@soundcloud.com>
2018-09-17 17:43:49 +0200

    post build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
+
+# Post Build Script
+
+set -e # Exit immediately if a command exits with a non-zero status (failure)
+
+echo "**************************************************************************************************"
+echo "Post Build Script"
+echo "**************************************************************************************************"
+
+##################################################
+# Start UI Tests
+##################################################
+
+
 appcenter test run espresso --app "shivendratiwari81-lmt0/HelloAppCenterAndroid" --devices 249b175a --app-path app/build/outputs/apk/debug/app-debug.apk  --test-series "master" --locale "en_US" --build-dir app/build/outputs/apk/androidTest/debug/
+
+
+echo ""
+echo "**************************************************************************************************"
+echo "Post Build Script complete"
+echo "**************************************************************************************************"
+
+
